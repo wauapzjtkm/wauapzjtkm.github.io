f066228304b4ca2358c727de72d4658c2a3d7cf2
wauapzjtkm <104789849+wauapzjtkm@users.noreply.github.com>
2022-05-02 23:22:13 +0200

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -3,7 +3,7 @@ const step = 1;
 
 function outNum(num, elem) {
    let l = document.querySelector('#' + elem);
-   n = 57;
+   n = 157;
    let t = Math.round(time/(num/step));
    let interval = setInterval(() => {
       n = n + step;
@@ -15,4 +15,4 @@ function outNum(num, elem) {
       t);
 }
 
-outNum(89, 'out-1');
+outNum(198, 'out-1');
